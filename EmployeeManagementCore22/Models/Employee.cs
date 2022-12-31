04136229bf172b942f238586de3635003ffc655c
carloswm85 <carloswashingtonmercado@gmail.com>
2022-12-30 21:38:51 -0300

    Up to video 43, Select list validation in asp net core

--- EmployeeManagementCore22/Models/Employee.cs
+++ EmployeeManagementCore22/Models/Employee.cs
@@ -5,13 +5,21 @@ namespace EmployeeManagementCore22.Models
     public class Employee
     {
         public int Id { get; set; }
+        
         [Required]
         [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
         public string Name { get; set; }
+        
         [Required]
         [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid email format.")]
         [Display(Name = "Office Email")]
         public string Email { get; set; }
-        public Dept Department { get; set; }
+
+        [Required]
+        // Dept only, displays validation error: The value '' is invalid, because value for "Please Select" in option-select is empty.
+        // Dept Required only, displays validation error: The value '' is invalid. n fact, required has no effect, since Enum data type are inherently required.
+        // Dept ? only, displays validation error: None, and the Enum data type is made OPTIONAL
+        // Dept ? and Required, displays CORRECT validation error: The Department field is required.
+        public Dept? Department { get; set; } 
     }
 }
