5c670c6ab7245028b60ff7dc8783b2de83fd6d79
carloswm85 <carloswashingtonmercado@gmail.com>
2023-01-07 10:20:30 -0300

    Up to video 46, Install entity framework core in visual studio

---
+++ EmployeeManagementCore22.DataAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementCore22.DataAccess
+{
+    public class Class1
+    {
+    }
+}
