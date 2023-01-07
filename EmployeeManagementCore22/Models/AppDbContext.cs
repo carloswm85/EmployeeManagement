8dfba74066ac0c2793c416a508fd8583b6b15cce
carloswm85 <carloswashingtonmercado@gmail.com>
2023-01-07 10:37:37 -0300

    Up to video 47, DbContext in entity framework core

---
+++ EmployeeManagementCore22/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementCore22.Models
+{
+    public class AppDbContext : DbContext
+    {
+        // Configure: (1) DB Provider, and (2) connection string
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        // Include a DB set property for each type in the project (Employee.cs)
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
