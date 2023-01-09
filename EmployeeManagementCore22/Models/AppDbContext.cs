b0a82a128104aca712378bcfc4b5080886a9db1f
carloswm85 <carloswashingtonmercado@gmail.com>
2023-01-09 10:15:35 -0300

    Up to video 51, Entity framework core seed data

--- EmployeeManagementCore22/Models/AppDbContext.cs
+++ EmployeeManagementCore22/Models/AppDbContext.cs
@@ -9,10 +9,24 @@ namespace EmployeeManagementCore22.Models
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
-
+            // Nothing
         }
 
         // Include a DB set property for each type in the project (Employee.cs)
         public DbSet<Employee> Employees { get; set; }
+
+        // Seeding the Database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+        }
     }
 }
+
+// EFC MIGRATIONS (also works with dot-net core cli)
+/*
+ * Package Manager Console:
+ * - get-help about_entityframeworkcore
+ * - add-migration NameOfTheMigration
+ * - update-database
+ */
---
+++ EmployeeManagementCore22/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementCore22.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        // This is an extension method for Microsoft.EntityFrameworkCore.ModelBuilder
+        // It is used in AppDbContext.cs
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Mary",
+                    Department = Dept.IT,
+                    Email = "mary@pragimtech.com"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "John",
+                    Department = Dept.HR,
+                    Email = "john@pragimtech.com"
+                }
+            );
+        }
+    }
+}
